b3ffb9218d1258e305b34f2b1995222e714e400e
Jascha Ehrenreich <jascha@jaeh.at>
2018-07-02 16:58:30 +0200

    update requirements to use newest api version

--- includes/admin/requirements.php
+++ includes/admin/requirements.php
@@ -1,41 +1,16 @@
 <?php
 /**
- * @package    Magic-Appointments
+ * @package    Magic-User-Admin
  * @version    0.0.1
- * @author     Thomas Griffin, Gary Jones, Juliette Reinders Folmer
- * @copyright  Copyright (c) 2011, Thomas Griffin
- * @license    http://opensource.org/licenses/gpl-2.0.php GPL v2 or later
  */
 
 require_once plugin_dir_path( __FILE__ ) . '../fallback/plugin-activation.php';
 
-add_action( 'tgmpa_register', 'magic_user_admin_register_required_plugins' );
+add_action( 'magic_pa_register', 'magic_user_admin_register_required_plugins' );
 
-/**
- * Register the required plugins for this theme.
- *
- * In this example, we register five plugins:
- * - one included with the TGMPA library
- * - two from an external source, one from an arbitrary source, one from a GitHub repository
- * - two from the .org repo, where one demonstrates the use of the `is_callable` argument
- *
- * The variables passed to the `tgmpa()` function should be:
- * - an array of plugin arrays;
- * - optionally a configuration array.
- * If you are not changing anything in the configuration array, you can remove the array and remove the
- * variable from the function call: `tgmpa( $plugins );`.
- * In that case, the TGMPA default settings will be used.
- *
- * This function is hooked into `tgmpa_register`, which is fired on the WP `init` action on priority 10.
- */
 function magic_user_admin_register_required_plugins() {
-	/*
-	 * Array of plugin arrays. Required keys are name and slug.
-	 * If the source is NOT from the .org repo, then source is also required.
-	 */
 
  	$plugins = array(
- 		// This is an example of how to include a plugin from the WordPress Plugin Repository.
 	 	array(
 	 		'name'      => 'Timber',
 	 		'slug'      => 'timber-library',
@@ -53,22 +28,25 @@ function magic_user_admin_register_required_plugins() {
 	 		'slug'      => 'acf-field-date-time-picker',
 	 		'required'  => true,
 	 	),
+
+	 	array(
+	 		'name'      => 'WP Less',
+	 		'slug'      => 'wp-less',
+	 		'required'  => true,
+	 	),
 	);
-	/*
-	 * Array of configuration settings. Amend each line as needed.
-	 */
 	$config = array(
-		'id'           => 'magic_user_admin',                 // Unique ID for hashing notices for multiple instances of TGMPA.
-		'default_path' => '',                      // Default absolute path to bundled plugins.
-		'menu'         => 'tgmpa-install-plugins', // Menu slug.
-		'parent_slug'  => 'plugins.php',            // Parent menu slug.
-		'capability'   => 'manage_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
-		'has_notices'  => true,                    // Show admin notices or not.
-		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
-		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
-		'message'      => '',                      // Message to output right before the plugins table.
+		'id'           => 'magic_user_admin',         // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                         // Default absolute path to bundled plugins.
+		'menu'         => 'magic_pa-install-plugins', // Menu slug.
+		'parent_slug'  => 'plugins.php',              // Parent menu slug.
+		'capability'   => 'manage_options',           // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'has_notices'  => true,                       // Show admin notices or not.
+		'dismissable'  => true,                       // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                         // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                      // Automatically activate plugins after installation or not.
+		'message'      => '',                         // Message to output right before the plugins table.
 	);
 
-	tgmpa( $plugins, $config );
+	magic_pa( $plugins, $config );
 }
