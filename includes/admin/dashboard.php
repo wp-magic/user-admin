9045068a75076970f78fde66555bba46f1c01005
Jascha Ehrenreich <jascha@jaeh.at>
2018-07-06 08:14:56 +0200

    use globals

--- includes/admin/dashboard.php
+++ includes/admin/dashboard.php
@@ -5,56 +5,56 @@ add_action( 'admin_menu', function () {
 
   $settings = array(
     array(
-      'name' => 'page_header',
+      'name' => MAGIC_USER_ADMIN_SLUG . '_page_header',
       'type' => 'header',
       'value' => 'Page urls',
       'label' => 'Where page templates are registered in your child theme',
     ),
     array(
-      'name' => 'magic_user_admin_login_page',
+      'name' => MAGIC_USER_ADMIN_SLUG . '_login_page',
       'type' => 'text',
       'default' => '/login',
       'label' => 'Login Page',
     ),
     array(
-      'name' => 'magic_user_admin_registration_page',
+      'name' => MAGIC_USER_ADMIN_SLUG . '_registration_page',
       'type' => 'text',
       'default' => '/register',
       'label' => 'Registration Page',
     ),
     array(
-      'name' => 'magic_user_admin_logout_page',
+      'name' => MAGIC_USER_ADMIN_SLUG . '_logout_page',
       'type' => 'text',
       'default' => '/logout',
       'label' => 'Logout Page',
     ),
     array(
-      'name' => 'magic_user_admin_profile_page',
+      'name' => MAGIC_USER_ADMIN_SLUG . '_profile_page',
       'type' => 'text',
       'default' => '/profile',
       'label' => 'Profile Page',
     ),
 
     array(
-      'name' => 'redirect_header',
+      'name' => MAGIC_USER_ADMIN_SLUG . '_redirect_header',
       'type' => 'header',
       'value' => 'Redirect urls',
       'label' => 'Where the user gets redirected after certain actions',
     ),
     array(
-      'name' => 'magic_user_admin_login_redirect',
+      'name' => MAGIC_USER_ADMIN_SLUG . '_login_redirect',
       'type' => 'text',
       'default' => '/',
       'label' => 'Login Redirect',
     ),
     array(
-      'name' => 'magic_user_admin_registration_redirect',
+      'name' => MAGIC_USER_ADMIN_SLUG . '_registration_redirect',
       'type' => 'text',
       'default' => '/',
       'label' => 'Registration Redirect',
     ),
     array(
-      'name' => 'magic_user_admin_logout_redirect',
+      'name' => MAGIC_USER_ADMIN_SLUG . '_logout_redirect',
       'type' => 'text',
       'default' => '/',
       'label' => 'Logout Redirect',
