48162b7013977f16306a6c7245851750c9dac142
Jascha Ehrenreich <jascha@jaeh.at>
2018-07-08 16:57:29 +0200

    move page-template registration into action

--- includes/plugin.php
+++ includes/plugin.php
@@ -6,7 +6,6 @@
  * @license   GPL-2.0+
  */
 
-require_once 'custom-fields/index.php';
 require_once 'styles/index.php';
 
 require_once 'admin/hide-admin-bar.php';
@@ -17,13 +16,17 @@ if ( is_admin() ) {
 	require_once 'admin/requirements.php';
 }
 
-if ( function_exists( 'magic_page_templates' ) ) {
-	$templates = array(
-		MAGIC_USER_ADMIN_LOGIN_TEMPLATE => 'Login Page',
-		MAGIC_USER_ADMIN_LOGOUT_TEMPLATE  => 'Logout Page',
-		MAGIC_USER_ADMIN_REGISTRATION_TEMPLATE => 'Registration Page',
-		MAGIC_USER_ADMIN_ACCOUNT_TEMPLATE => 'User Account Page',
-	);
+add_action( 'plugins_loaded', function () {
+	if ( function_exists( 'magic_page_templates' ) ) {
+		$templates = array(
+			MAGIC_USER_ADMIN_LOGIN_TEMPLATE => 'Login Page',
+			MAGIC_USER_ADMIN_LOGOUT_TEMPLATE  => 'Logout Page',
+			MAGIC_USER_ADMIN_REGISTRATION_TEMPLATE => 'Registration Page',
+			MAGIC_USER_ADMIN_ACCOUNT_TEMPLATE => 'User Account Page',
+		);
 
-	magic_page_templates($templates, plugin_dir_path( __FILE__ ) . 'templates/' );
-}
+		magic_page_templates($templates, plugin_dir_path( __FILE__ ) . 'templates/' );
+
+		require_once 'custom-fields/index.php';
+	}
+} );
